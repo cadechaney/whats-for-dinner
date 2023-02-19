daae5923dda7fbb06a02f714ac73901d92f74fb2
Cade Chaney <cadechaney@live.com>
2023-02-18 23:05:07 -0600

    Add html skeleton ajustments, js code and new css updates

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 
 // query Selectors
     // Radio Buttons
-var sidesRadio = document.querySelector("#side")
-var mainDishRadio = document.querySelector("#Main-Dish")
-var dessertRadio = document.querySelector("#dessert")
+
     //Lets cook button
-var letsCookButton = document.querySelector("button")
+var letsCookButton = document.querySelector(".let-cook")
+var clearButton = document.querySelector(".clear-button")
+var cookPotImage = document.querySelector(".cookPot-img")
+var secondBox = document.querySelector(".second-box")
+
 
 
 
@@ -13,10 +15,11 @@ var letsCookButton = document.querySelector("button")
 
 // Event Listeners
     // Radio Buttons
- sidesRadio.addEventListener(null, randomSide)
+//  sidesRadio.addEventListener(null, randomSide)
 //  mainDishRadio.addEventListener("click")
     // Lets Cook Button
-letsCookButton.addEventListener("click", displayFood)
+letsCookButton.addEventListener("click", randomRecipe)
+clearButton.addEventListener('click', clearRecipe)
 
 //Food Item Arrays
 sides = [
@@ -31,19 +34,111 @@ sides = [
 "Garlic Butter Mushrooms",
 "Hush Puppies"]
 
+main = [
+    "Spaghetti and Meatballs",
+    "Pineapple Chicken",
+    "Shakshuka",
+    "Thai Yellow Curry",
+    "Bibimbap",
+    "Chicken Parmesean",
+    "Butternut Squash Soup",
+    "BBQ Chicken Burgers",
+    "Ramen",
+    "Empanadas",
+    "Chicken Fried Rice",
+    "Sheet Pan Fajitas",
+    "Margarita Pizza"]
+
+dessert = [ "Apple Pie",
+    "Lemon Meringue Pie",
+    "Black Forest Cake",
+    "Banana Bread",
+    "Peach Cobbler",
+    "Cheesecake",
+    "Funfetti Cake",
+    "Baklava",
+    "Flan",
+    "Macarons",
+    "Macaroons",
+    "Chocolate Cupcakes",
+    "Pavlova",
+    "Pumpkin Pie",
+    "Key Lime Pie",
+    "Tart Tatin",
+    "Croissants",
+    "Eclairs"]
 
 
-function randomSide() {
-    sides[getRandom(sides)];
+// getelementbyname
+
+
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
+
+  // use for and if else statements to get the random 
+//   var radioOptions = document.getElementsByName("radio");
+
+function randomRecipe() {
+    clearButton.classList.remove("view")
+    var radioOptions = document.getElementsByName("radio");
+    for (var i = 0; i < radioOptions.length; i++) {
+        if (radioOptions[i].checked) {
+            radioOptions[i].checked = false
+            console.log("OHHHH YEAH")
+            var mealOption = (radioOptions[i].value)
+        }
+    }
+ if (mealOption === 'Sides') {
+    secondBox.innerHTML = `
+    <article class = "dish">
+        <p class= "You-should-make"> You Should Make: </p>
+        <p class= "Dish-name"> ${sides[getRandomIndex(sides)]}!</p>
+    </article>
+    `
+ }  else if  (mealOption === 'Main Dishes') {
+    secondBox.innerHTML = `
+    <article class = "dish">
+        <p class= "You-should-make"> You Should Make: </p>
+        <p class= "Dish-name"> ${main[getRandomIndex(main)]}!</p>
+    </article>
+    `
+ }  else if  (mealOption === 'Desserts') {
+    secondBox.innerHTML = `
+    <article class = "dish">
+        <p class= "You-should-make"> You Should Make: </p>
+        <p class= "Dish-name"> ${dessert[getRandomIndex(dessert)]}!</p>
+    </article>
+    `
+ }  else if  (mealOption === 'entire meal') {
+    secondBox.innerHTML = `
+    <article class = "dish">
+        <p class= "You-should-make"> You Should Make: </p>
+        <p class= "Dish-name"> ${main[getRandomIndex(main)]} with a side of ${sides[getRandomIndex(sides)]} and ${dessert[getRandomIndex(dessert)]} for dessert!</p>
+    </article>
+    `
+
+
+}
 }
 
+function clearRecipe() {
+    clearButton.classList.add("hidden");
+    secondBox.innerHTML = `
+    <img class="cookPot-img" src='assets/cookpot.svg' width="175" height="175">
+    `
 
+}
+
+ 
+     
 
 
 //the display food button needs to be a conditional that calls one of 
 // the other functions
-function displayFood() {
- console.log(sides)
-console.log(randomSide())
-}
+// function displayFood() {
+// //  console.log(sides)
+// // console.log(sides[getRandomIndex(sides)])
+// console.log(randomRecipe())
 
